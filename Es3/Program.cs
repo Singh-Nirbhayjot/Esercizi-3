3a44143f697757b3946f8bd484f28b9b2a2ad971
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2026-02-04 22:31:35 +0100

    Aggiungere i file di progetto.

---
+++ Es3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+
+namespace Es3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Inserisci una parola: ");
+            string parola = ReadLine().ToLower();
+
+            int contatore = 0;
+            bool a = false, e = false, i = false, o = false, u = false;
+
+            for (int j = 0; j < parola.Length; j++)
+            {
+                char c = parola[j];
+
+                if (c == 'a' && !a)
+                {
+                    contatore++;
+                    a = true;
+                }
+                else if (c == 'e' && !e)
+                {
+                    contatore++;
+                    e = true;
+                }
+                else if (c == 'i' && !i)
+                {
+                    contatore++;
+                    i = true;
+                }
+                else if (c == 'o' && !o)
+                {
+                    contatore++;
+                    o = true;
+                }
+                else if (c == 'u' && !u)
+                {
+                    contatore++;
+                    u = true;
+                }
+            }
+
+            if (contatore >= 4)
+                WriteLine("Parola magica ");
+            else
+                WriteLine("Parola non magica");
+        }
+    }
+}
