93ad1a4b33b8fdfffb598223893b317818a0d66a
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2026-02-04 22:40:45 +0100

    Esercizio 2

---
+++ Es2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace Es4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Inserisci numero: ");
+            string n = ReadLine();
+
+            bool sicura = true;
+
+            for (int i = 1; i < n.Length - 1; i++)
+            {
+                if (!((n[i] > n[i - 1] && n[i] > n[i + 1]) ||
+                      (n[i] < n[i - 1] && n[i] < n[i + 1])))
+                {
+                    sicura = false;
+                    break;
+                }
+            }
+
+            if (sicura)
+                WriteLine("Strada sicura");
+            else
+                WriteLine("Strada NON sicura");
+        }
+    }
+}
