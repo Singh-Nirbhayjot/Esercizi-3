bcca819162e6e04966528c60ae0fc1c4f4b4953b
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2026-02-04 22:35:33 +0100

    Aggiunto Es1

---
+++ Es1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace Es1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Write("Inserisci numero: ");
+            int.TryParse(ReadLine(), out n);
+
+            int somma = 0;
+            int prodotto = 1;
+
+            int temp = n;
+            while (temp > 0)
+            {
+                int cifra = temp % 10;
+                somma += cifra;
+                prodotto *= cifra;
+                temp /= 10;
+            }
+
+            if (somma % 3 == 0 && prodotto % 2 == 0)
+                WriteLine("Codice valido");
+            else
+                WriteLine("Codice NON valido");
+        }
+    }
+}
